7b64f640b98fb830bdd725be5f3c431818825b04
MaximilianoLedesma09 <maxiledesma882@gmail.com>
2026-09-22 18:05:35 -0300

    config--deploy--09222026

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // El site debe ser tu nombre de usuario, no la dirección del repositorio
+  site: 'https://MaximilianoLedesma09.github.io',
+  // El base debe ser el nombre del repositorio, con una barra al inicio
+  base: '/Anubit',
+});
+
+
